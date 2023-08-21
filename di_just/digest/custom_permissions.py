059f06509294e2c60c85b2b5d775a48852f689c2
Krivova <qrvmil@yandex.ru>
2023-08-21 10:26:40 +0300

    Fix: LinkDigest delete

--- di_just/digest/custom_permissions.py
+++ di_just/digest/custom_permissions.py
@@ -2,19 +2,13 @@ from rest_framework import permissions
 
 
 class IsOwner(permissions.BasePermission):
-    """
-    Object-level permission to only allow owners of an object to edit it.
-    Assumes the model instance has an `owner` attribute.
-    """
-
     message = "Allowed only for owner"
 
     def has_object_permission(self, request, view, obj):
-        return obj == request.user
+        return obj.owner.user == request.user
 
 
 class IsCommenter(permissions.BasePermission):
-
     message = "Allowed only for owner"
 
     def has_object_permission(self, request, view, obj):
